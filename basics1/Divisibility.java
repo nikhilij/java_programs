ff1294a850a6ef203e7505da1f1c4cc0d2501ed1
nikhilij <roynikhil973@gmail.com>
2024-01-28 14:44:35 +0530

    added divisiblity

--- basics1/Divisibility.java
+++ basics1/Divisibility.java
@@ -1,14 +1,39 @@
 public class Divisibility {
     public static void main(String[] args) {
+        System.out.println("Divided by 3:");
+        printDivisibleBy3();
+        
+        System.out.println("\nDivided by 5:");
+        printDivisibleBy5();
+        
+        System.out.println("\nDivided by 3 & 5:");
+        printDivisibleBy3And5();
+    }
+
+    private static void printDivisibleBy3() {
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i + " is divisible by both 3 and 5");
-            } else if (i % 3 == 0) {
-                System.out.println(i + " is divisible by 3");
-            } else if (i % 5 == 0) {
-                System.out.println(i + " is divisible by 5");
+            if (i % 3 == 0) {
+                System.out.print(i + ", ");
             }
         }
+        System.out.println();
+    }
+
+    private static void printDivisibleBy5() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printDivisibleBy3And5() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println();
     }
 }
-
